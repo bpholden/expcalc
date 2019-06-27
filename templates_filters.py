f7310797adfbc905932a7feb229825296fa51b32
Bradford Holden <brad.p.holden@gmail.com>
2019-06-26 21:31:32 -0700

    removed unneeded templates

--- templates_filters.py
+++ templates_filters.py
@@ -64,25 +64,16 @@ def get_star_templates():
     tabbr = [
              "G5V","K0V","K5V","M5V","O5V","B5V","A0V","A5V","F5V","SNe Type 1a 10 days"
              ]
-    templates = ["",
-        "s0_template.fits",
-        "elliptical_template.fits",
-        "orion_template.fits",
-        "pn_template.fits",
-        "sa_template.fits",
-        "sb_template.fits",
-        "sc_template.fits",	      
-        "starb1_template.fits",
-        "qso_template.fits",
-        "sn1a10d_template.fits",
+    templates = [
+        "G5V_pickles_27.fits",
+        "K0V_pickles_32.fits",
+        "K5V_pickles_36.fits", 
+        "M5V_pickles_44.fits",
         "O5V_pickles_1.fits",
         "B5V_pickles_6.fits",
         "A0V_pickles_9.fits",
         "A5V_pickles_12.fits",
         "F5V_pickles_16.fits",
-        "G5V_pickles_27.fits",
-        "K0V_pickles_32.fits",
-        "K5V_pickles_36.fits", 
-        "M5V_pickles_44.fits"
+        "sn1a10d_template.fits"
         ]	      
     return(templates,tabbr)
