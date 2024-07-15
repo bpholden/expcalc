001ab68bf24622114eb739382b011bd2cfd9e8a3
Brad Holden <brad.p.holden@gmail.com>
2024-07-15 14:18:35 -0700

    print

--- test_if_up.py
+++ test_if_up.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from optparse import OptionParser
 import subprocess as sub
 import asciitable
@@ -22,17 +23,17 @@ if testing:
 exec_str = "%s/tab_s2n" % (etchostname)
 exec_str += '?slitwidth=0.75&binning=1x1&exptime=3600.0&mag=17.0&ffilter=sdss_r.dat&mtype=2&seeing=0.75&template=s0_template.fits&inst=esi&airmass=1.1&redshift=0.0'
 if verbose:
-    print exec_str
+    print(exec_str)
 
 com = 'curl "' + exec_str +'"'
 if verbose:
-    print com
+    print(com)
 retval = sub.check_output(com,shell=True)
 if verbose:
-    print retval
+    print(retval)
 refdata = asciitable.read("test_results.csv")
 retvals = retval.split('\n')
 if len(retvals) == (len(refdata)+2):
-    print "Success: system up and working"
+    print("Success: system up and working")
 else:
-    print "Return data not correct"
+    print("Return data not correct")
